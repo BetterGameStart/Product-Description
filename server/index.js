ea030696af4ca8d4c0cf033506492009bda83449
Austin Smith <asmit2222@gmail.com>
2020-05-21 13:59:16 -0400

    implemented crud functions

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const faker = require('faker');
 const db = require('./database/index.js');
+
+let imageCounter = 1;
+
+const getRandomImages = () => {
+  const results = [];
+  const limit = faker.random.number({ min: 4, max: 7 });
+
+  for (let j = 0; j < limit; j += 1) {
+    const imgURL = `https://aragorn-images.s3-us-west-2.amazonaws.com/${imageCounter}.jpg`;
+    results.push(imgURL);
+    imageCounter += 1;
+    if (imageCounter > 30) {
+      imageCounter = 1;
+    }
+  }
+  return results;
+};
 
 const app = express();
 const PORT = 3004;
@@ -26,6 +44,71 @@ app.get('/api/games/:id', (req, res) => {
       res.end();
     }
   });
+});
+
+app.post('/api/games/:id', (req, res) => {
+  res.sendStatus(200);
+  const ID = Number(path.basename(req.url));
+  if (ID < 0 || ID > 100) {
+    ID = 1;
+  }
+  const fillData = () => {
+    const data = [];
+    const newGame = new db.Game({
+      id: ID,
+      name: faker.commerce.productName(),
+      details: faker.lorem.paragraphs(3),
+      images: getRandomImages(),
+    });
+    data.push(newGame);
+    return data;
+  };
+  async function seed() {
+    const data = fillData();
+    for (const game of data) {
+      await db.save(game);
+    }
+    console.log('success');
+  }
+  seed();
+});
+
+app.delete('/api/games/:id', (req, res) => {
+  res.sendStatus(200);
+  const ID = Number(path.basename(req.url));
+
+  const myquery = { id: ID };
+
+  db.Game.deleteMany(myquery, (err, obj) => {
+    if (err) throw err;
+    console.log('deleted');
+  });
+});
+
+app.put('/api/games/:id', (req, res) => {
+  res.sendStatus(200);
+  const ID = Number(path.basename(req.url));
+
+  const myquery = { id: ID };
+
+  const update = {
+    $set: {
+      id: ID,
+      name: faker.commerce.productName(),
+      details: faker.lorem.paragraphs(3),
+      images: getRandomImages(),
+    },
+  };
+
+  const options = { upsert: true };
+
+  db.Game.updateOne(myquery, update, options)
+    .then((result) => {
+      const { matchedCount, modifiedCount } = result;
+      if (matchedCount && modifiedCount) {
+        console.log('Successfully updated');
+      }
+    });
 });
 
 app.listen(PORT, () => {
